c4b7ab84f71ca0c47a8b691b43365a0d84e9cbf3
patel gaytri <patel_gaytri2005@yahoo.com>
2021-02-01 20:22:16 -0500

    commit demo app

---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function NoMatch(props){
+    console.log(props);
+
+  
+    return(
+     <div className="NoMatch">
+      404 notFound!
+     </div>
+    );
+}
+
+export default NoMatch;
